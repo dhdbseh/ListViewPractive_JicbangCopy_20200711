2ccc14ed6d0e086ab7d4e866fb094ccd17dc1dac
yd27 <dhdbseh5oe@naver.com>
2020-07-12 10:49:59 +0900

    전화연결 버튼 이벤트 구현했는데 아직 못했어

--- app/src/main/java/kr/co/tjoeun/listviewpractive_jicbangcopy_20200711/datas/ViewRoomDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractive_jicbangcopy_20200711/datas/ViewRoomDetailActivity.kt
@@ -1,6 +1,21 @@
 package kr.co.tjoeun.listviewpractive_jicbangcopy_20200711.datas
 
+import android.content.Intent
+
 class ViewRoomDetailActivity {
+
+    override fun setupEvents(){
+
+        dialBtn.setOnClickListener {
+
+            //폰번은 010-1111-2222 인걸로 설정
+
+            val myUri = Uri.parse("tel:010-1111-2222")
+            val myIntent = Intent(Intent)
+
+        }
+
+    }
 
     override fun setValues() {
 

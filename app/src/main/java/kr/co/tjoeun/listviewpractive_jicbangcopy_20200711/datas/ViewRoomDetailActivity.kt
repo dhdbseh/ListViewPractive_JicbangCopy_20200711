91027ab8ff678cfa2dd654b3c055f0bb31ef0dff
yd27 <dhdbseh5oe@naver.com>
2020-07-12 10:46:56 +0900

    넘겨준 방 데이터를 실제로 뿌려주기 (아 어떻게 하는거야)

--- app/src/main/java/kr/co/tjoeun/listviewpractive_jicbangcopy_20200711/datas/ViewRoomDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractive_jicbangcopy_20200711/datas/ViewRoomDetailActivity.kt
@@ -2,11 +2,16 @@ package kr.co.tjoeun.listviewpractive_jicbangcopy_20200711.datas
 
 class ViewRoomDetailActivity {
 
-    override fun setValues(){
+    override fun setValues() {
 
         val roomData = intent.getserializableExtra("room") as Room
 
+        priceTxt.text = roomData.getFormattedPrice()
+        descriptionTxt.text = roomData.description
 
+        addressTxt.text = roomData.address
+        floorTxt.text = roomData.getFormattedFloor()
 
+    }
 
 }
